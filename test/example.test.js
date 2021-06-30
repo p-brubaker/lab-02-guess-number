c1ccb3fce246debcc8b5c7c45c137db64c4ee3ca
Paul Brubaker <pbrubak@gmail.com>
2021-06-30 13:41:52 -0700

    write failing tests for compareNumber function

--- test/example.test.js
+++ test/example.test.js
@@ -3,16 +3,20 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
+test('Tell if a number is too high', (expect) => {
+    const expected = 1;
+    const actual = compareNumbers(10, 20);
     expect.equal(actual, expected);
 });
+
+test('Tell if a number is too low', (expect) => {
+    const expected = -1;
+    const actual = compareNumbers(20, 5);
+    expect.equal(actual, expected);
+});
+
+test('Tell if a number is equal', (expect) => {
+    const expected = 0;
+    const actual = compareNumbers(50, 50);
+    expect.equal(actual, expected);
+})
