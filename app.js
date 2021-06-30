40461edd1bb333faffaa5a3de980803d5a0cf486
Paul Brubaker <pbrubak@gmail.com>
2021-06-30 16:21:56 -0700

    refactor event listeners out of function

--- app.js
+++ app.js
@@ -10,38 +10,34 @@ let guessesRemaining;
 let numberToGuess;
 let guessOutcome;
 
+guessInput.addEventListener('click', () => guessInput.value = '');
+submitGuess.addEventListener('click', () => { 
+    guessesRemaining--;
+    displayGuesses.textContent = guessesRemaining;
+    guessOutcome = compareNumbers(numberToGuess, guessInput.value);
+    console.log(guessOutcome);
+    if (guessOutcome === 'correct') {
+        result.textContent = `You won in ${7 - guessesRemaining} guesses! Nice job! Care to play again?`;
+        displayGuesses.textContent = '7';
+        guessNumber();
+    } 
+    else if (guessesRemaining === 0) {
+        result.textContent = `Out of guesses! The correct number was ${numberToGuess}. Why not give it another try?`
+        displayGuesses.textContent = '7';
+        guessNumber();
+    }
+    else if (guessOutcome === 1) {
+        result.textContent = `${guessInput.value} is too high! Guess again`;
+    } 
+    else if (guessOutcome === -1) {
+        result.textContent = `${guessInput.value} is too low! Guess again`;
+    }
+});
 
-function guessNumber(isRoundOne) {
-
+function guessNumber() {
     numberToGuess = Math.ceil(Math.random()*100);
     guessesRemaining = 7;
-    
-    if (isRoundOne) {
-        guessInput.addEventListener('click', () => guessInput.value = '');
-        submitGuess.addEventListener('click', () => { 
-            guessesRemaining--;
-            displayGuesses.textContent = guessesRemaining;
-            guessOutcome = compareNumbers(numberToGuess, guessInput.value);
-            console.log(guessOutcome);
-            if (guessOutcome === 'correct') {
-                result.textContent = `You won in ${7 - guessesRemaining} guesses! Nice job! Care to play again?`;
-                displayGuesses.textContent = '7';
-                guessNumber(false);
-            } 
-            else if (guessesRemaining === 0) {
-                result.textContent = `Out of guesses! The correct number was ${numberToGuess}. Why not give it another try?`
-                displayGuesses.textContent = '7';
-                guessNumber(false);
-            }
-            else if (guessOutcome === 1) {
-                result.textContent = `${guessInput.value} is too high! Guess again`;
-            } 
-            else if (guessOutcome === -1) {
-                result.textContent = `${guessInput.value} is too low! Guess again`;
-            }
-        });
-    }
 }
 
-guessNumber(true);
+guessNumber();
 
